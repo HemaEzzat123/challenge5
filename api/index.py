37d8e2686d3f2f41a89e893a3861e1b7408fa294
IbrahimEzzat3 <eng.ibrahim.ezzat.03@gmail.com>
2025-05-05 22:42:27 +0300

    first

--- api/index.py
+++ api/index.py
@@ -1,19 +1,27 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Form
 from fastapi.responses import HTMLResponse
-import os
+import os, base64, traceback
 
 app = FastAPI()
 
 @app.get("/", response_class=HTMLResponse)
-async def root():
-    flag = os.getenv("FLAG", "FLAG NOT FOUND")
-    return HTMLResponse(content=f"<h1>Hello from FastAPI!</h1><p>Flag: {flag}</p>")
+async def login_page():
+    return HTMLResponse("""
+        <form action="/login" method="post">
+            <input type="text" name="username" required>
+            <input type="password" name="password" required>
+            <input type="submit" value="submit">
+        </form>
+    """)
 
 @app.post("/login")
 async def login(username: str = Form(...), password: str = Form(...)):
-    if username == "admin" and password == "password123":
-        flag = os.getenv("FLAG", "FLAG NOT FOUND")
-        response = HTMLResponse(content="ACCESS GRANTED<br><img src='/flag.png'/>")
-        response.set_cookie(key="SESSIONID", value=base64.b64encode(flag.encode()).decode())
-        return response
-    return HTMLResponse(content="ACCESS DENIED", status_code=403)
+    try:
+        if username == "admin" and password == "password123":
+            flag = os.getenv("FLAG", "FLAG NOT FOUND")
+            res = HTMLResponse(content="ACCESS GRANTED<br><img src='/flag.png'/>")
+            res.set_cookie(key="SESSIONID", value=base64.b64encode(flag.encode()).decode())
+            return res
+        return HTMLResponse(content="ACCESS DENIED", status_code=403)
+    except Exception:
+        return HTMLResponse(content=f"<pre>{traceback.format_exc()}</pre>", status_code=500)
